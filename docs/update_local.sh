9167d19dee2f3e9bdd332a88dfc085d498700941
Yifei Ding <dingyifeiair@gmail.com>
2021-04-28 02:45:12 +0800

    Generate files for zh-hans and zh-hant

--- docs/update_local.sh
+++ docs/update_local.sh
@@ -1,5 +1,7 @@
-
-
+#!/bin/bash
+#Automatically generate a set of po and md files for Localization
+#in the ./local/[language]/ folder
+#requirement: pip install mdpo
 
 for dir in ./local/*/
 do
@@ -7,8 +9,10 @@ do
     echo "Updating ${dir##*/}"
     for file in *.md; do
       echo "Converting $file to ${file//.md/.po}"
-      md2po $file --md-encoding utf-8 --po-encoding utf-8 -e utf-8 -w 71 -q -s --po-filepath local/${dir##*/}/${file//.md/.po}
+      md2po $file --md-encoding utf-8 --po-encoding utf-8 \
+      -e utf-8 -w 71 -q -s --po-filepath local/${dir##*/}/${file//.md/.po}
       echo "Converting ${file//.md/.po} to $file"
-      po2md $file --md-encoding utf-8 --po-encoding utf-8 --pofiles local/${dir##*/}/${file//.md/.po} -q -s local/${dir##*/}/$file
+      po2md $file --md-encoding utf-8 --po-encoding utf-8 \
+      --pofiles local/${dir##*/}/${file//.md/.po} -q -s local/${dir##*/}/$file
     done
 done
