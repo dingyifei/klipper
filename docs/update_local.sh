f130830044f3a4396e5651f211551cb686fc58df
Yifei Ding <dingyifeiair@gmail.com>
2021-04-28 02:35:35 +0800

    add the capability to update all the local folders at once

--- docs/update_local.sh
+++ docs/update_local.sh
@@ -1,8 +1,14 @@
 
-for file in *.md; do
-  echo "Converting $file to ${file//.md/.po}"
-  md2po $file --md-encoding utf-8 --po-encoding utf-8 -e utf-8 -w 71 -q -s --po-filepath local/zh-hans/${file//.md/.po}
-  echo "Converting ${file//.md/.po} to $file"
-  po2md $file --md-encoding utf-8 --po-encoding utf-8 --pofiles local/zh-hans/${file//.md/.po} -q -s local/zh-hans/$file
-done
 
+
+for dir in ./local/*/
+do
+    dir=${dir%*/}
+    echo "Updating ${dir##*/}"
+    for file in *.md; do
+      echo "Converting $file to ${file//.md/.po}"
+      md2po $file --md-encoding utf-8 --po-encoding utf-8 -e utf-8 -w 71 -q -s --po-filepath local/${dir##*/}/${file//.md/.po}
+      echo "Converting ${file//.md/.po} to $file"
+      po2md $file --md-encoding utf-8 --po-encoding utf-8 --pofiles local/${dir##*/}/${file//.md/.po} -q -s local/${dir##*/}/$file
+    done
+done
