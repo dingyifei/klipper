6f9591573873ca547d242909b6cfdd0698e45918
Yifei Ding <dingyifeiair@gmail.com>
2021-05-05 04:33:18 +0800

    Localization: Add Comments To Script
    Signed-off-by: Yifei Ding <yifeiding@protonmail.com>

--- scripts/update_locales.sh
+++ scripts/update_locales.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-#Automatically generate a set of po and md files for Localization
-#in the ./locales/[language]/ folder
+#Automatically generate a set of po and md files for 
+#Localization in the ./locales/[language]/ folder
 #requirement: pip install mdpo
 
 width=71
 
+#Generate Pot Base Files
 for file in docs/*.md; do
-  mdfile=${file/docs\//}
-  potfile=${mdfile//.md/.pot}
-  cat docs/$mdfile
+  
+  mdfile=${file/docs\//} #Remove docs/
+  mdfilepath=docs/$mdfile #add docs/ 
+  potfile=${mdfile//.md/.pot} #replace .md with .pot
+  potfilepath=docs/locales/en/$potfile #Add docs/locales/en/ to potfile
   echo "Converting $mdfile to $potfile"
-  command="md2po docs/$mdfile --md-encoding utf-8 --po-encoding utf-8 -e utf-8 \
-  -w $width -q -s --po-filepath docs/locales/en/$potfile"
+  command="md2po $mdfilepath --md-encoding utf-8 --po-encoding utf-8 -e utf-8 \
+  -w $width -q -s --po-filepath $potfilepath"
   echo "$command"
   $($command)
 done
@@ -22,16 +25,20 @@ for dir in docs/locales/*/; do
   if [ $dir != "en" ]; then
     echo "Updating $dir"
     for file in docs/*.md; do
-      mdfile=${file/docs\//}
-      pofile=${mdfile//.md/.po}
+      mdfile=${file/docs\//} #Remove docs/
+      mdfilepath=docs/$mdfile #add docs/ 
+      pofile=${mdfile//.md/.po} #replace .md with .po
+      pofilepath=docs/locales/$dir/LC_MESSAGES/$pofile #Add target directory 
+      #to pofile
+      targetmdfile=docs/locales/$dir/$mdfile #Add target directory to mdfile
       echo "Converting $mdfile to $pofile"
-      command="md2po docs/$mdfile --md-encoding utf-8 --po-encoding utf-8 \
-      -e utf-8 -w $width -q -s --po-filepath docs/locales/$dir/LC_MESSAGES/$pofile"
+      command="md2po $mdfilepath --md-encoding utf-8 --po-encoding utf-8 \
+      -e utf-8 -w $width -q -s --po-filepath $pofilepath"
       echo "$command"
       $($command)
       echo "Converting $pofile to $mdfile"
-      command="po2md docs/$mdfile --md-encoding utf-8 --po-encoding utf-8 \
-      -p docs/locales/$dir/LC_MESSAGES/$pofile -q -s docs/locales/$dir/$mdfile"
+      command="po2md $mdfilepath --md-encoding utf-8 --po-encoding utf-8 \
+      -p $pofilepath -q -s $targetmdfile"
       echo "$command"
       $($command)
     done
